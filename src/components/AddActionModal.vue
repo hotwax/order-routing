fa72a861bf527a6b8f6ea536cf1d0e4c6ffd76b5
Disha Talreja <dishatalreja1202@gmail.com>
2022-10-03 16:57:50 +0530

    Implemented static UI for order routing rules(#2vjxcqb)

---
+++ src/components/AddActionModal.vue
@@ -0,0 +1,80 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-button @click="closeModal">
+          <ion-icon slot="icon-only" :icon="closeOutline" />
+        </ion-button>
+      </ion-buttons>
+      <ion-title>{{ $t("Add actions") }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  
+  <ion-content>
+    <ion-searchbar :placeholder="$t('Search')" />
+    
+    <ion-list>
+      <ion-item>
+        <ion-label>Route orders</ion-label>
+        <ion-button fill="outline" color="medium">{{ $t("Add") }}</ion-button>
+      </ion-item>
+      <ion-item>
+        <ion-label>Re-broker orders</ion-label>
+        <ion-button fill="outline" color="medium">{{ $t("Add") }}</ion-button>
+      </ion-item>
+      <ion-item>
+        <ion-label>Load pre-order category</ion-label>
+        <ion-button fill="outline" color="medium">{{ $t("Add") }}</ion-button>
+      </ion-item>
+    </ion-list>
+  </ion-content>
+</template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+  modalController,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { closeOutline } from 'ionicons/icons';
+import { useStore } from "@/store";
+
+export default defineComponent({
+  name: "Modal",
+  components: {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonIcon,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonSearchbar,
+    IonTitle,
+    IonToolbar,
+  },
+  methods: {
+    closeModal() {
+      modalController.dismiss({ dismissed: true });
+    }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      closeOutline,
+      store,
+    };
+  },
+});
+</script>
---
+++ src/components/RuleConfiguration.vue
@@ -0,0 +1,120 @@
+<template>
+  <section>
+    <ion-card>
+      <ion-card-header>
+        <ion-card-title>
+          {{ $t("Start") }}
+        </ion-card-title>
+      </ion-card-header>
+      <ion-item>
+        <ion-label>{{ $t("Run on") }}</ion-label>
+        <ion-select>Trigger</ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label>{{ $t("Trigger") }}</ion-label>
+        <ion-select>Release Pre-orders</ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label>{{ $t("Condition") }}</ion-label>
+        <ion-select>Completion</ion-select>
+      </ion-item>
+    </ion-card>
+    <ion-card>
+      <ion-accordion-group>
+        <ion-item>
+          <h3>Route Orders</h3>
+          <ion-button slot="end" fill="clear" color="medium" @click="ruleOptions">
+            <ion-icon slot="icon-only" :icon="ellipsisVerticalOutline" />
+          </ion-button>
+        </ion-item>
+        <ion-accordion>
+          <ion-item slot="header">
+            <ion-label>{{ $t("View parameters") }}</ion-label>
+          </ion-item>
+          <div slot="content">
+            <ion-item>
+              <ion-label>ShipmentId: 12345678</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>ShipmentTypeId: PUR_SHIP</ion-label>
+            </ion-item>
+          </div>
+        </ion-accordion>
+      </ion-accordion-group>
+    </ion-card>
+    <div class="center-align">
+      <ion-button @click="addAction" color="light">
+        <ion-icon slot="start" :icon="addOutline" />
+        {{ $t("Add Action") }}
+      </ion-button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { IonAccordionGroup, IonAccordion, IonButton, IonCard, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonSelect, IonIcon, popoverController, modalController  } from '@ionic/vue';
+import { addOutline, ellipsisVerticalOutline } from "ionicons/icons";
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import RulePopover from '@/components/RulePopover.vue'
+import AddActionModal from '@/components/AddActionModal.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonAccordionGroup,
+    IonAccordion,
+    IonButton,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonSelect
+  },
+  methods: {
+    async ruleOptions(){
+      const popover = await popoverController
+        .create({
+          component: RulePopover,
+          translucent: true,
+          showBackdrop: true,
+        })
+      return popover.present();
+    },
+    async addAction() {
+      const modal = await modalController
+        .create({
+          component: AddActionModal
+        })
+        modal.onDidDismiss()
+      return modal.present();
+    },
+  },
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+    return {
+      addOutline,
+      ellipsisVerticalOutline,
+      store,
+      router
+    };
+  }
+});
+</script>
+<style scoped>
+@media (min-width: 991px) {  
+  section {
+    overflow: hidden;
+    border: var(--border-medium);
+    border-radius: 16px;
+  }
+  .center-align {
+    display: flex;
+    justify-content: center;
+  }
+}  
+</style>

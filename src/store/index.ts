7366117b25e631556fda8c9d8df751f780058f65
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-01-13 14:20:26 +0530

    Implemented: support to register the order routing module(#17)

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,7 @@ import RootState from "./RootState"
 import createPersistedState from "vuex-persistedstate";
 import userModule from "./modules/user";
 import utilModule from "./modules/util"
+import orderRoutingModule from "./modules/orderRouting"
 
 // TODO check how to register it from the components only
 // Handle same module registering multiple time on page refresh
@@ -27,7 +28,8 @@ const store = createStore<RootState>({
   plugins: [ persistState ],
   modules: {
     "user": userModule,
-    "util": utilModule
+    "util": utilModule,
+    "orderRouting": orderRoutingModule
   },
 })
 

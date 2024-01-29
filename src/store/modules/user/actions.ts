c161c6b3b42a100f802a612d6c9065985cb8369a
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-01-29 18:14:04 +0530

    Added: loader when performing login action(#46)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -6,6 +6,7 @@ import * as types from "./mutation-types"
 import { hasError, showToast } from "@/utils"
 import { translate } from "@/i18n"
 import logger from "@/logger"
+import emitter from "@/event-bus"
 
 const actions: ActionTree<UserState, RootState> = {
 
@@ -14,19 +15,26 @@ const actions: ActionTree<UserState, RootState> = {
   */
   async login({ commit }, { username, password }) {
     try {
+      if(!username.length || !password.length) {
+        return Promise.reject('')
+      }
+
+      emitter.emit("presentLoader", { message: "Logging in...", backdropDismiss: false })
       // TODO: implement support for permission check
       const token = await UserService.login(username, password)
 
       const userProfile = await UserService.getUserProfile(token);
 
-      // Check if we need to fetch only associated product stores of user
+      // TODO: fetch only associated product stores for user, currently api does not support this
       userProfile.stores = await UserService.getEComStores(token);
 
       commit(types.USER_TOKEN_CHANGED, { newToken: token })
       commit(types.USER_INFO_UPDATED, userProfile);
       commit(types.USER_CURRENT_ECOM_STORE_UPDATED, userProfile.stores.length ? userProfile.stores[0] : {});
+      emitter.emit("dismissLoader")
       return Promise.resolve({ token })
     } catch (err: any) {
+      emitter.emit("dismissLoader")
       showToast(translate(err));
       logger.error("error", err);
       return Promise.reject(new Error(err))

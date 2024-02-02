0b478745c151bdb570cca8f82ea3848a24122099
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-02-02 19:19:34 +0530

    Implemented: confirmation alert when disabling a schedule(#46)

--- src/views/BrokeringRoute.vue
+++ src/views/BrokeringRoute.vue
@@ -96,7 +96,7 @@
             </ion-card>
             <div class="actions desktop-only">
               <div>
-                <ion-button :disabled="typeof isOmsConnectionExist === 'boolean' && !isOmsConnectionExist" size="small" fill="outline" color="danger" @click="disable">{{ translate("Disable") }}</ion-button>
+                <ion-button :disabled="job.paused === 'Y' || typeof isOmsConnectionExist === 'boolean' && !isOmsConnectionExist" size="small" fill="outline" color="danger" @click="disable">{{ translate("Disable") }}</ion-button>
               </div>
               <div>
                 <ion-button :disabled="typeof isOmsConnectionExist === 'boolean' && !isOmsConnectionExist" size="small" fill="outline" @click="saveChanges()">{{ translate("Save changes") }}</ion-button>
@@ -250,22 +250,36 @@ async function saveChanges() {
 }
 
 async function disable() {
-  const payload = {
-    routingGroupId: props.routingGroupId,
-    paused: "Y"  // setting Y to disable the job
-  }
+  const alert = await alertController
+    .create({
+      header: translate("Disable"),
+      message: translate("Disabling this schedule will cancel this occurrence and all following occurrences. This schedule will have to be re-enabled manually to run it again."),
+      buttons: [{
+        text: translate("Don't cancel"),
+        role: "cancel"
+      }, {
+        text: translate("Cancel"),
+        handler: async () => {
+          const payload = {
+            routingGroupId: props.routingGroupId,
+            paused: "Y"  // setting Y to disable the schedule
+          }
 
-  try {
-    const resp = await OrderRoutingService.scheduleBrokering(payload)
-    if(!hasError(resp)){
-      showToast(translate("Job disabled"))
-    } else {
-      throw resp.data
-    }
-  } catch(err) {
-    showToast(translate("Failed to update job"))
-    logger.error(err)
-  }
+          try {
+            const resp = await OrderRoutingService.scheduleBrokering(payload)
+            if(!hasError(resp)){
+              showToast(translate("Schedule disabled"))
+            } else {
+              throw resp.data
+            }
+          } catch(err) {
+            showToast(translate("Failed to update schedule"))
+            logger.error(err)
+          }
+        }
+      }],
+    });
+  return alert.present();
 }
 
 async function runNow() {

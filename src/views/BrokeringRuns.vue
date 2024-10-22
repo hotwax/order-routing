c7c9ba92cc562a67a3fa0feee4650e3112b666da
Nabhi Jain <98026895+coolnj4@users.noreply.github.com>
2024-10-22 11:39:20 +0530

    removed un-used imports

--- src/views/BrokeringRuns.vue
+++ src/views/BrokeringRuns.vue
@@ -91,7 +91,7 @@ import { translate } from "@/i18n";
 import { Group } from "@/types";
 import { getDateAndTime, showToast } from "@/utils";
 import { IonBadge, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonRadioGroup, IonRadio, IonSpinner, IonTitle, IonToolbar, alertController, onIonViewWillEnter, popoverController } from "@ionic/vue";
-import { addOutline, arrowForwardOutline, ellipsisVerticalOutline } from "ionicons/icons"
+import { addOutline, ellipsisVerticalOutline } from "ionicons/icons"
 import { DateTime } from "luxon";
 import { computed, ref } from "vue";
 import { useRouter } from "vue-router";

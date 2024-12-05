f17437cea15abcd7424c03f750fa95cf531bab64
R-Sourabh <rsourabh002@gmail.com>
2024-12-05 15:49:10 +0530

    Improved: corrected the type error for shipment threshold enum(#260)

--- src/views/BrokeringQuery.vue
+++ src/views/BrokeringQuery.vue
@@ -288,9 +288,9 @@
                     {{ translate("Turn of the facility order limit check") }}
                   </ion-toggle>
                 </ion-item>
-                <ion-item v-if="getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, 'SHIP_THREHOLD')">
+                <ion-item v-if="getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, 'SHIP_THRESHOLD')">
                   <ion-label>{{ translate('Shipment threshold check') }}</ion-label>
-                  <ion-chip slot="end" outline @click="selectValue('SHIP_THREHOLD', 'Add shipment threshold check')">{{ getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THREHOLD").fieldValue || getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THREHOLD").fieldValue == 0 ? getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THREHOLD").fieldValue : "-" }}</ion-chip>
+                  <ion-chip slot="end" outline @click="selectValue('SHIP_THRESHOLD', 'Add shipment threshold check')">{{ getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THRESHOLD").fieldValue || getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THRESHOLD").fieldValue == 0 ? getFilterValue(inventoryRuleFilterOptions, conditionFilterEnums, "SHIP_THRESHOLD").fieldValue : "-" }}</ion-chip>
                 </ion-item>
               </ion-card>
               <ion-card>

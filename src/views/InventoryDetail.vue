34d02912a99700656ac57eea68caae26dc45ef5d
Aditya Patel <heyadityapatel@gmail.com>
2026-06-20 12:50:24 +0530

    Use list item rows for inventory logs

--- src/views/InventoryDetail.vue
+++ src/views/InventoryDetail.vue
@@ -81,77 +81,71 @@
       </div>
       <section class="ion-margin panel logs-panel">
         <div class="logs-scroll">
-          <ion-list class="logs-list">
-            <ion-item lines="full">
-              <div class="logs-row">
-                <ion-label>
-                  <p>{{ translate("Id") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("Date Time Received") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("Facility Id") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("Location Seq Id") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("Comments") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("ATP diff") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("QOH Diff") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("ATP Total") }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ translate("QOH Total") }}</p>
-                </ion-label>
-              </div>
-            </ion-item>
+          <div class="list-item">
+            <ion-label>
+              <p>{{ translate("Id") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("Date Time Received") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("Facility Id") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("Location Seq Id") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("Comments") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("ATP diff") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("QOH Diff") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("ATP Total") }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ translate("QOH Total") }}</p>
+            </ion-label>
+          </div>
 
-            <ion-item lines="full" v-for="(log, index) in inventoryLogs" :key="`${log.inventoryItemId || 'inventory-log'}-${log.effectiveDate || index}-${index}`">
-              <div class="logs-row">
-                <ion-label>
-                  <p>{{ log.inventoryItemId }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ formatDateTime(log.effectiveDate) }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ log.facilityId }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ log.locationSeqId }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ log.description || "-" }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ log.availableToPromiseDiff }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ log.quantityOnHandDiff }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ quantityTotal(log.lastAvailableToPromise, log.availableToPromiseDiff) }}</p>
-                </ion-label>
-                <ion-label>
-                  <p>{{ quantityTotal(log.lastQuantityOnHand, log.quantityOnHandDiff) }}</p>
-                </ion-label>
-              </div>
-            </ion-item>
+          <div class="list-item" v-for="(log, index) in inventoryLogs" :key="`${log.inventoryItemId || 'inventory-log'}-${log.effectiveDate || index}-${index}`">
+            <ion-label>
+              <p>{{ log.inventoryItemId }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ formatDateTime(log.effectiveDate) }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ log.facilityId }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ log.locationSeqId }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ log.description || "-" }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ log.availableToPromiseDiff }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ log.quantityOnHandDiff }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ quantityTotal(log.lastAvailableToPromise, log.availableToPromiseDiff) }}</p>
+            </ion-label>
+            <ion-label>
+              <p>{{ quantityTotal(log.lastQuantityOnHand, log.quantityOnHandDiff) }}</p>
+            </ion-label>
+          </div>
 
-            <ion-item v-if="!inventoryLogs.length" lines="none" class="logs-empty">
-              <ion-label class="logs-empty-label">
-                {{ translate("No inventory logs found") }}
-              </ion-label>
-            </ion-item>
-          </ion-list>
+          <div v-if="!inventoryLogs.length" class="list-item logs-empty">
+            <ion-label>
+              <p>{{ translate("No inventory logs found") }}</p>
+            </ion-label>
+          </div>
         </div>
       </section>
     </ion-content>
@@ -172,7 +166,6 @@ import {
   IonHeader,
   IonItem,
   IonLabel,
-  IonList,
   IonPage,
   IonSelect,
   IonSelectOption,
@@ -379,7 +372,6 @@ ion-note[slot="end"] {
 }
 
 .logs-panel {
-  display: block;
   margin-top: var(--spacer-base, 16px);
 }
 
@@ -393,44 +385,32 @@ ion-badge {
   width: 100%;
 }
 
-.logs-list {
-  min-width: 1004px;
-}
-
-.logs-row {
+.list-item {
   display: grid;
-  grid-template-columns:
-    minmax(110px, 1fr)
-    minmax(155px, 1.3fr)
-    minmax(100px, .9fr)
-    minmax(120px, 1fr)
-    minmax(160px, 1.3fr)
-    minmax(70px, .7fr)
-    minmax(75px, .7fr)
-    minmax(75px, .7fr)
-    minmax(75px, .7fr);
-  gap: var(--spacer-xs, 8px);
   align-items: center;
-  width: 100%;
+  justify-items: center;
+  --columns-desktop: 9;
+  --col-calc: var(--columns-desktop);
+  --implicit-columns: calc(var(--col-calc) - 1);
+  grid-template-columns: repeat(var(--implicit-columns), minmax(0, 1fr)) minmax(75px, max-content);
+  border-bottom: 1px solid var(--ion-color-medium);
+  gap: var(--spacer-xs, 8px);
   min-width: 1004px;
 }
 
-.logs-row ion-label {
+.list-item ion-label {
   min-width: 0;
 }
 
-.logs-row p {
+.list-item p {
   margin: 0;
   overflow-wrap: anywhere;
   white-space: normal;
 }
 
 .logs-empty {
-  --min-height: 96px;
-  min-width: 1004px;
-}
-
-.logs-empty-label {
+  grid-template-columns: 1fr;
+  min-height: 96px;
   text-align: center;
 }
 
@@ -442,6 +422,14 @@ ion-badge {
 @media (max-width: 768px) {
   .detail-layout {
     grid-template-columns: 1fr;
+  }
+}
+
+@media (min-width: 991px) {
+  .list-item {
+    --col-calc: var(--columns-desktop);
+    padding-block: var(--spacer-sm);
+    padding-inline: var(--spacer-sm);
   }
 }
 

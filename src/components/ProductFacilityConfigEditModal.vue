8da60a95e9a835bb38ec7ff5508b14bea0a2172a
Vaibhavi Upreti <vaibhavi.upreti@hotwax.co>
2026-06-22 16:17:34 +0530

    fixed config modal to restrict and validate against negative/decimal values for safety stock and days to ship

--- src/components/ProductFacilityConfigEditModal.vue
+++ src/components/ProductFacilityConfigEditModal.vue
@@ -25,10 +25,10 @@
         </ion-select>
       </ion-item>
       <ion-item>
-        <ion-input type="number" min="0" placeholder="0" v-model="minimumStock" label="Safety stock"></ion-input>
+        <ion-input type="number" min="0" placeholder="0" v-model="minimumStock" label="Safety stock" @keydown="restrictDecimalAndNegative"></ion-input>
       </ion-item>
       <ion-item>
-        <ion-input type="number" min="0" placeholder="0" v-model="daysToShip" label="Days to Ship"></ion-input>
+        <ion-input type="number" min="0" placeholder="0" v-model="daysToShip" label="Days to Ship" @keydown="restrictDecimalAndNegative"></ion-input>
       </ion-item>
     </ion-list>
   </ion-content>
@@ -57,6 +57,13 @@ function closeModal() {
   modalController.dismiss();
 }
 
+function restrictDecimalAndNegative(event: KeyboardEvent) {
+  const keys = ['e', 'E', '-', '+', '.'];
+  if (keys.includes(event.key)) {
+    event.preventDefault();
+  }
+}
+
 async function updateConfig() {
   const params = {
     facilityId: props.selectedFacility
@@ -71,11 +78,21 @@ async function updateConfig() {
   }
 
   if(minimumStock.value === 0 || minimumStock.value) {
-    params["minimumStock"] = minimumStock.value
+    const minStockVal = Number(minimumStock.value);
+    if (minStockVal < 0 || !Number.isInteger(minStockVal)) {
+      commonUtil.showToast(translate("Safety stock must be a non-negative integer."));
+      return;
+    }
+    params["minimumStock"] = minStockVal;
   }
 
   if(daysToShip.value === 0 || daysToShip.value) {
-    params["daysToShip"] = daysToShip.value
+    const daysToShipVal = Number(daysToShip.value);
+    if (daysToShipVal < 0 || !Number.isInteger(daysToShipVal)) {
+      commonUtil.showToast(translate("Days to Ship must be a non-negative integer."));
+      return;
+    }
+    params["daysToShip"] = daysToShipVal;
   }
 
   const selectedProductIds = props.selectedProducts.map((product: any) => product.productId)

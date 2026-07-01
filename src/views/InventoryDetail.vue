e94cb3d82b506c1c4fb1384ff5439690c55d8e30
Aditya Patel <heyadityapatel@gmail.com>
2026-07-01 12:17:02 +0530

    Fix inventory history delta balances

--- src/views/InventoryDetail.vue
+++ src/views/InventoryDetail.vue
@@ -206,17 +206,13 @@
                   <div class="balance-cell">
                     <span class="overline">{{ translate("ATP") }}</span>
                     <span class="balance-flow">
-                      <span>{{ m.raw.lastAvailableToPromise ?? 0 }}</span>
-                      <ion-icon :icon="arrowForwardOutline" />
-                      <strong>{{ runningTotal(m.raw.lastAvailableToPromise, m.raw.availableToPromiseDiff) }}</strong>
+                      <span :class="diffClass(m.raw.availableToPromiseDiff)">{{ signed(m.raw.availableToPromiseDiff) }}</span>
                     </span>
                   </div>
                   <div class="balance-cell">
                     <span class="overline">{{ translate("QOH") }}</span>
                     <span class="balance-flow">
-                      <span>{{ m.raw.lastQuantityOnHand ?? 0 }}</span>
-                      <ion-icon :icon="arrowForwardOutline" />
-                      <strong>{{ runningTotal(m.raw.lastQuantityOnHand, m.raw.quantityOnHandDiff) }}</strong>
+                      <span :class="diffClass(m.raw.quantityOnHandDiff)">{{ signed(m.raw.quantityOnHandDiff) }}</span>
                     </span>
                   </div>
                   <div class="balance-cell" v-if="m.raw.accountingQuantityDiff !== undefined && m.raw.accountingQuantityDiff !== null">
@@ -497,10 +493,6 @@ function facilityName(facilityId: string) {
 function signed(value: any) {
   const num = Number(value) || 0
   return num > 0 ? `+${num}` : `${num}`
-}
-
-function runningTotal(last: any, diff: any) {
-  return (Number(last) || 0) + (Number(diff) || 0)
 }
 
 function diffClass(value: any) {

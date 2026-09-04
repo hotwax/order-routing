6e929636c000a43d60c33203163dcdf20ca8cd08
Aditya Patel <heyadityapatel@gmail.com>
2026-09-03 20:40:56 -0700

    refactor(common): simplify App footer using self-contained DxpOmsInstanceFooter

--- src/App.vue
+++ src/App.vue
@@ -82,7 +82,6 @@
           :instance-label="instanceUrl"
           :product-stores="productStores"
           :current-product-store-id="currentProductStore.productStoreId"
-          :time-zone="userProfile?.timeZone"
           @update:product-store="(productStoreId, ionEvent) => setProductStore(ionEvent)"
         />
       </ion-menu>
